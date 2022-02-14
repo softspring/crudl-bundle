110dddadaf7d4e3db787d0d3f9277858b5a1e0e0
Javi H. Gil <javihgil@softspring.es>
2022-02-14 12:38:39 +0100

    Improve list filter form configuration

--- src/Form/EntityListFilterForm.php
+++ src/Form/EntityListFilterForm.php
@@ -29,58 +29,105 @@ class EntityListFilterForm extends AbstractType implements EntityListFilterFormI
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add(self::getOrderFieldParamName(), HiddenType::class, [
+        $builder->add(static::getOrderFieldParamName(), HiddenType::class, [
             'mapped' => false,
         ]);
 
-        $builder->add(self::getOrderDirectionParamName(), HiddenType::class, [
+        $builder->add(static::getOrderDirectionParamName(), HiddenType::class, [
             'mapped' => false,
         ]);
 
-        $builder->add(self::getRppParamName(), HiddenType::class, [
+        $builder->add(static::getRppParamName(), HiddenType::class, [
             'mapped' => false,
         ]);
     }
 
     public function getPage(Request $request): int
     {
-        return (int) $request->query->get(self::getPageParamName(), 1);
+        return (int) $request->query->get(static::getPageParamName(), 1);
     }
 
     public function getRpp(Request $request): int
     {
-        return (int) $request->query->get(self::getRppParamName(), 50);
+        return (int) $request->query->get(static::getRppParamName(), static::rppDefault());
     }
 
     public function getOrder(Request $request): array
     {
         if (class_exists(RequestParam::class)) {
-            $order = RequestParam::getQueryValidParam($request, self::getOrderFieldParamName(), 'id', ['id']);
-            $sort = RequestParam::getQueryValidParam($request, self::getOrderDirectionParamName(), 'asc', ['asc', 'desc']);
+            $order = RequestParam::getQueryValidParam($request, static::getOrderFieldParamName(), static::orderDefaultField(), static::orderValidFields());
+            $sort = RequestParam::getQueryValidParam($request, static::getOrderDirectionParamName(), 'asc', ['asc', 'desc']);
 
             return [$order => $sort];
         }
 
-        return [$request->query->get(self::getOrderFieldParamName(), '') ?: 'id' => $request->query->get(self::getOrderDirectionParamName(), '') ?: 'asc'];
+        return [$request->query->get(static::getOrderFieldParamName(), '') ?: 'id' => $request->query->get(static::getOrderDirectionParamName(), '') ?: 'asc'];
     }
 
+    /**
+     * @deprecated use pageParamName() instead
+     */
     public static function getPageParamName(): string
+    {
+        return static::pageParamName();
+    }
+
+    public static function pageParamName(): string
     {
         return 'page';
     }
 
+    /**
+     * @deprecated use rppParamName() instead
+     */
     public static function getRppParamName(): string
+    {
+        return static::rppParamName();
+    }
+
+    public static function rppParamName(): string
     {
         return 'rpp';
     }
 
+    public static function rppDefault(): int
+    {
+        return 50;
+    }
+
+    /**
+     * @deprecated use orderFieldParamName() instead
+     */
     public static function getOrderFieldParamName(): string
+    {
+        return static::orderFieldParamName();
+    }
+
+    public static function orderFieldParamName(): string
     {
         return 'sort';
     }
 
+    /**
+     * @deprecated use orderDirectionParamName() instead
+     */
     public static function getOrderDirectionParamName(): string
     {
+        return static::orderDirectionParamName();
+    }
+
+    public static function orderDirectionParamName(): string
+    {
         return 'order';
+    }
+
+    public static function orderValidFields(): array
+    {
+        return ['id'];
+    }
+
+    public static function orderDefaultField(): string
+    {
+        return 'id';
     }
 }
